f0ff6898cc0f2514258cd67e2a24e51f171f7c3f
vitamaxoduol <vitamaxoduo@gmail.com>
2023-07-19 16:26:14 +0300

    Completed assignment

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -7,20 +7,47 @@ function ColorList() {
     "salmon",
     "darkslategray",
     "hotpink",
+    "blue",
+    "red",
+    "black",
+    "green",
   ];
+
+  const colorElements = colors.map((color) => {
+    return <li key={color} style={{color: color}}>{color}</li>
+  });
 
   return (
     <div>
       <h1>Top 5 CSS Colors</h1>
       <ol>
-        <li style={{ color: colors[0] }}>{colors[0]}</li>
+         {/* display the array of <li> elements here! */}
+        {/* <li style={{ color: colors[0] }}>{colors[0]}</li>
         <li style={{ color: colors[1] }}>{colors[1]}</li>
         <li style={{ color: colors[2] }}>{colors[2]}</li>
         <li style={{ color: colors[3] }}>{colors[3]}</li>
         <li style={{ color: colors[4] }}>{colors[4]}</li>
+        <li style={{ color: colors[5] }}>{colors[5]}</li> */}
+        {colorElements}
       </ol>
     </div>
   );
 }
 
 export default ColorList;
+
+/*const todoItems = todos.map((todo) =>
+  <li key={todo.id}>
+    {todo.text}
+  </li>
+);
+*/
+
+/*const todoItems = todos.map((todo, index) =>
+  // Only do this if items have no stable IDs
+  <li key={index}>
+    {todo.text}
+  </li>
+);
+
+*/
